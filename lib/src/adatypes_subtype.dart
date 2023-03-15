9549b106902f620f2e6425d5e3812cad4ac1ecaa
shamblett <steve.hamblett@linux.com>
2023-03-15 15:20:53 +0000

    Addition working

--- lib/src/adatypes_subtype.dart
+++ lib/src/adatypes_subtype.dart
@@ -15,9 +15,54 @@ abstract class Subtype {
   final int start;
 
   int _value = 0;
+  int get value => _value;
 
-  void call(int val) => _value = val;
+  void call(int val) => _value = _boundsCheck(val);
 
   @override
   String toString() => _value.toString();
+
+  @override
+  bool operator ==(Object other) {
+    if (other is int) {
+      if (_value == other) {
+        return true;
+      }
+    }
+    if (other is Subtype) {
+      if (_value == other.value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  dynamic operator +(Object other) {
+    if (other is int) {
+      _boundsCheck(_value += other);
+    } else {
+      try {
+        _boundsCheck(_value += (other as Subtype).value);
+      } on ArgumentError {
+        rethrow;
+      } catch (e) {
+        throw ArgumentError(
+            'AdaTypes:Subtype - the type supplied is not derived from Subtype - $e',
+            'other');
+      }
+    }
+    return this;
+  }
+
+  @override
+  int get hashCode => _value.hashCode;
+
+  int _boundsCheck(int val) {
+    if (val < start || val > end) {
+      throw ArgumentError(
+          'AdaTypes:Subtype - the value supplied is not in the range of the subtype',
+          'value');
+    }
+    return val;
+  }
 }
--- test/adatypes_subtype_test.dart
+++ test/adatypes_subtype_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: unrelated_type_equality_checks
 
 import 'package:test/test.dart';
 import 'package:adatypes/adatypes.dart';
@@ -15,13 +16,68 @@ typedef PeasInPod = AScore;
 
 void main() {
   test('Type equivalence', () {
-    var numEggs = EggsInBox();
+    final numEggs = EggsInBox();
     numEggs(4);
-    var numPeas = PeasInPod();
+    final numPeas = PeasInPod();
     numPeas(15);
     Type peasType = PeasInPod;
     Type eggsType = EggsInBox;
     expect(peasType == eggsType, isFalse);
-    print('Number of peas is $numPeas');
+  });
+
+  test('Range check on initialisation', () {
+    final numEggs = EggsInBox();
+    var exceptionRaised = false;
+    try {
+      numEggs(40);
+    } on ArgumentError catch (e) {
+      exceptionRaised = true;
+      print(e);
+    }
+    expect(exceptionRaised, isTrue);
+    expect(numEggs.value, 0);
+  });
+
+  test('Equality', () {
+    final numEggs1 = EggsInBox();
+    numEggs1(10);
+    final numEggs2 = EggsInBox();
+    numEggs2(10);
+    expect(numEggs1 == numEggs2, isTrue);
+    expect(numEggs1 == 10, isTrue);
+    expect(numEggs2 == 40, isFalse);
+  });
+
+  test('Addition', () {
+    var exceptionRaised = false;
+    var numEggs1 = EggsInBox();
+    numEggs1(3);
+    var numEggs2 = EggsInBox();
+    numEggs2(4);
+    numEggs1 += numEggs2;
+    numEggs2 += 5;
+    expect(numEggs1 == 7, isTrue);
+    expect(numEggs2 == 9, isTrue);
+    try {
+      numEggs2 += 10;
+    } on ArgumentError catch (e) {
+      exceptionRaised = true;
+      print('Subtype + integer - $e');
+    }
+    try {
+      numEggs2 += numEggs1;
+    } on ArgumentError catch (e) {
+      exceptionRaised = true;
+      print('Subtype + Subtype - $e');
+    }
+    expect(exceptionRaised, isTrue);
+    exceptionRaised = false;
+    try {
+      numEggs2 += 0.0;
+    } on ArgumentError catch (e) {
+      exceptionRaised = true;
+      print('Subtype + non Subtype - $e');
+    }
+    expect(exceptionRaised, isTrue);
   });
 }
